b4241f22d74fc5f62c6b23f5159fa0ceda892ad0
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-03-16 19:32:54 -0400

    Fix an issue with constructors, reorganize tests

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
-let handlers;
+let handlers, wrap;
 if (typeof Proxy !== 'undefined') {
   if (typeof Reflect === 'undefined') {
     require('harmony-reflect');
   }
   handlers = {
-    get: (target, property, receiver) => {
+    get (target, property) {
       if (property === 'inspect') {
-        return () => ('[chainable Promise]');
+        return () => '[chainable Promise]';
       }
       if (property === '_raw') {
         return target();
@@ -15,14 +15,16 @@ if (typeof Proxy !== 'undefined') {
       // If the Promise itself has the property ('then', 'catch', etc.), return the property itself, bound to the target.
       // However, wrap the result of calling this function. This allows wrappedPromise.then(something) to also be wrapped.
       if (property in target()) {
-        return new Proxy(target()[property].bind(target()), {apply: function () {
-          // Relying on the `arguments` object isn't ideal, but doing something like Reflect.apply(...arguments) leaks the
-          // `arguments` object from this function and and disables v8's optimization, so using indices is the better way to
-          // handle it. When rest parameters are usable without a runtime flag, it'll be possible to replace this with
-          // `function(...args) { return wrap(Reflect.apply(...args)) }`
-          return wrap(Reflect.apply(arguments[0], arguments[1], arguments[2]));
-        }});
-        return wrap(target()[property].bind(target()));
+        if (typeof target()[property] === 'function') {
+          return new Proxy(target()[property].bind(target()), {apply () {
+            // Relying on the `arguments` object isn't ideal, but doing something like Reflect.apply(...arguments) leaks the
+            // `arguments` object from this function and and disables v8's optimization, so using indices is the better way to
+            // handle it. When rest parameters are usable without a runtime flag, it'll be possible to replace this with
+            // `function(...args) { return wrap(Reflect.apply(...args)) }`
+            return wrap(Reflect.apply(arguments[0], arguments[1], arguments[2]));
+          }});
+        }
+        return target()[property];
       }
       // If the property has a value in the cache, use that value.
       if (target()._promise_chain_cache.hasOwnProperty(property)) {
@@ -43,7 +45,7 @@ if (typeof Proxy !== 'undefined') {
       }));
       return target()._promise_chain_cache[property];
     },
-    apply: (target, thisArg, args) => {
+    apply (target, thisArg, args) {
       // If the wrapped Promise is called, return a Promise that calls the result
       return wrap(Promise.all([target(), thisArg]).then(results => {
         if (typeof results[0] === 'function') {
@@ -55,7 +57,7 @@ if (typeof Proxy !== 'undefined') {
     construct: (target, args) => wrap(target().then(result => {
       // Ideally this would just be `new result(...args)` or `Reflect.construct(result, args)`, but node 4 doesn't support
       // the spread operator and harmony-reflect seems to have a bug with Reflect.construct().
-      return wrap(new (Function.prototype.bind.apply(result, args)));
+      return wrap(new (Function.prototype.bind.apply(result, [null].concat(args))));
     }))
   };
 
@@ -65,13 +67,13 @@ if (typeof Proxy !== 'undefined') {
   });
 }
 
-function wrap (target) {
-  if (typeof target === 'object' && target !== null && target.constructor.name === 'Promise' && typeof Proxy !== 'undefined') {
+wrap = function (target) {
+  if (typeof target === 'object' && target && target.constructor.name === 'Promise' && typeof Proxy !== 'undefined') {
     // The target needs to be stored internally as a function, so that it can use the `apply` and `construct` handlers.
     // (At the moment, v8 actually allows non-functions to use the `apply` trap, but that goes against the ES2015 spec. Also,
     // that behavior throws errors on any browser other than Chrome.)
     target._promise_chain_cache = {};
-    return new Proxy(() => (target), handlers);
+    return new Proxy(() => target, handlers);
   }
   return target;
 };
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
+/* global describe, beforeEach, it */
 'use strict';
 const wrap = require('.');
-const expect = require('chai').expect;
+const expect = require('chai').use(require('dirty-chai')).expect;
+const Bluebird = require('bluebird');
 
 const test_object = {
   regular_property: 'value1',
@@ -10,58 +12,51 @@ const test_object = {
     subproperty: 'hi! thanks for reading this source code!'
   },
   function_property: some_parameter => ({returned_property: some_parameter}),
-  function_returning_promise_property: param => (Promise.resolve(param))
-}
+  function_returning_promise_property: param => Promise.resolve(param)
+};
 
 describe('promise-chains', () => {
-  let prom, wrapped;
+  let wrapped;
   beforeEach(() => {
-    prom = Promise.resolve(test_object);
-    wrapped = wrap(prom);
+    wrapped = wrap(Promise.resolve(test_object));
   });
-  it('can use a wrapped promise with .then, like a regular promise', done => {
-    wrapped.then(result => {
-      expect(result).to.equal(test_object);
-      done();
-    }).catch(done);
-  });
-  it('can get a wrapped property', done => {
-    wrapped.regular_property.then(result => {
-      expect(result).to.equal(test_object.regular_property);
-      done();
-    }).catch(done);
-  });
-  it('resolves to undefined if given a nonexistent property', done => {
-    wrapped.nonexistent_property.then(result => {
-      expect(result).to.be.undefined;
-      done();
-    }).catch(done);
-  });
-  it('can call a function in a chain', done => {
-    wrapped.function_property('some text').then(result => {
-      expect(result).to.eql({returned_property: 'some text'});
-      done();
-    }).catch(done);
-  });
-  it('can call a function and return a wrapped promise', done => {
-    wrapped.function_property('some text').returned_property.then(result => {
-      expect(result).to.equal('some text');
-      done();
-    }).catch(done);
-  });
-  it('wraps any promises that are returned from functions', done => {
-    wrapped.function_returning_promise_property({prop: 'value'}).prop.then(result => {
-      expect(result).to.equal('value');
-      done();
-    }).catch(done);
-  });
+  it('can use a wrapped promise with .then, like a regular promise', Bluebird.coroutine(function *() {
+    expect(yield wrapped).to.equal(test_object);
+  }));
+  it('can get a wrapped property', Bluebird.coroutine(function *() {
+    expect(yield wrapped.regular_property).to.equal(test_object.regular_property);
+  }));
+  it('resolves to undefined if given a nonexistent property', Bluebird.coroutine(function *() {
+    expect(yield wrapped.nonexistent_property).to.be.undefined();
+  }));
+  it('can call a function in a chain', Bluebird.coroutine(function *() {
+    expect(yield wrapped.function_property('some text')).to.eql({returned_property: 'some text'});
+  }));
+  it('can call a function and return a wrapped promise', Bluebird.coroutine(function *() {
+    expect(yield wrapped.function_property('some text').returned_property).to.equal('some text');
+  }));
+  it('wraps any promises that are returned from functions', Bluebird.coroutine(function *() {
+    expect(yield wrapped.function_returning_promise_property({prop: 'value'}).prop).to.equal('value');
+  }));
   it('returns the same wrapped promise on multiple references to the same property', () => {
     expect(wrapped.unnecessarily_long_property_name).to.equal(wrapped.unnecessarily_long_property_name);
   });
-  it('returns a wrapped promise after a .then chain', done => {
-    wrapped.then(res => res).regular_property.then(result => {
-      expect(result).to.equal('value1');
-      done();
-    }).catch(done);
-  });
+  it('returns a wrapped promise after a .then chain', Bluebird.coroutine(function *() {
+    expect(yield wrapped.then(res => res).regular_property).to.equal('value1');
+  }));
+  it('allows wrapped promises for classes to be constructed', Bluebird.coroutine(function *() {
+    const Foo = class {
+      constructor (a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+      }
+    };
+    const FooPromise = wrap(Promise.resolve(Foo));
+    const result = yield new FooPromise(1, 2, 3);
+    expect(result.a).to.equal(1);
+    expect(result.b).to.equal(2);
+    expect(result.c).to.equal(3);
+    expect(result).to.be.an.instanceof(Foo);
+  }));
 });
